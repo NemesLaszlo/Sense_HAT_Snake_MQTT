26a334e6cbf3b6ffdf08872ad6b24df7a2494870
NemesLaszlo <wow.laszlo@gmail.com>
2020-04-16 21:54:40 +0200

    more comment and xml config files for the future mqtt feature

--- Snake.py
+++ Snake.py
@@ -5,8 +5,16 @@ from settings import *
 
 
 class Snake_game:
+    """
+    Snake Game main class, with the game parameters, and methods.
+    """
 
     def __init__(self):
+        """
+        Constructor of the Snake Game with the parameter of the Sense HAT panel and
+        with the basic parameters of the snake like direction and
+        the map parameters like apple position and pixels.
+        """
         self.sense = SenseHat()
         self.trail = [[3, 3]]
         self.direction = [1, 0]  # x y
@@ -16,6 +24,9 @@ class Snake_game:
 
     # 0 = up, 1 = right, 2 = down, 3 = left
     def set_direction(self, dir):
+        """
+        Snake Game direction setter, where set the snake moving direction.
+        """
         if dir == 0:
             self.direction = [0, -1]
         elif dir == 1:
@@ -27,6 +38,10 @@ class Snake_game:
 
     # game loop
     def play_game(self):
+        """
+        Main loop of the game with the direction event handler, plus
+        with the map border settings, movements and apple generator.
+        """
 
         while True:
             self.pixels = [clear] * 64
@@ -42,7 +57,7 @@ class Snake_game:
                         self.set_direction(2)
                     elif event.direction == "left":
                         self.set_direction(3)
-
+            # insert to the start of the array
             self.trail.insert(0, [self.trail[0][0] + self.direction[0], self.trail[0][1] + self.direction[1]])
 
             # one border cross in and the other off
@@ -55,6 +70,7 @@ class Snake_game:
             if self.trail[0][1] > 7:
                 self.trail[0][1] = 0
 
+            # we cover the situation, when the apple pos is a snake pos in the if statement
             if self.trail[0] == self.apple_pos:
                 self.apple_pos = []
                 while self.apple_pos == []:
@@ -67,12 +83,14 @@ class Snake_game:
                 self.length = 1
             else:
                 while len(self.trail) > self.length:
+                    # remove from the end ( "like" moving, but the length is correct)
                     self.trail.pop()
 
             for pos in self.trail:
+                # snake visualize on the pixel map (2d coord to 1d coord)
                 self.pixels[pos[1] * 8 + pos[0]] = white
 
-            # y * rowSize + x
+            # y * rowSize + x -> coordinate convert because of the pixel map
             self.pixels[self.apple_pos[1] * 8 + self.apple_pos[0]] = red
             # apple position (red led)
             self.sense.set_pixels(self.pixels)
